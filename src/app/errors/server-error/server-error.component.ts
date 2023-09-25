a0e1135b6871d92578a22d9937c0b5e4e5826153
albericopena <albericopena@gmail.com>
2023-09-25 16:58:04 -0300

    Error Handling

--- src/app/errors/server-error/server-error.component.ts
+++ src/app/errors/server-error/server-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-server-error',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./server-error.component.css']
 })
 export class ServerErrorComponent {
+  error: any;
+  /**
+   *
+   */
+  constructor(private router: Router) { 
+    const naviagation = this.router.getCurrentNavigation();
+    this.error = naviagation?.extras?.state?.['error'];
+    
+   }
 
 }
